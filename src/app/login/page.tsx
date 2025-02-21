1cfc9003408e817288f7649449da1179eb5aa11f
Thanh Nguyen <thanh.nguyencong@go1.com>
2025-02-21 19:09:40 +0700

    Add login and signup page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,201 @@
+'use client'
+
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import FormLabel from '@mui/material/FormLabel'
+import FormControl from '@mui/material/FormControl'
+import Link from '@mui/material/Link'
+import NextLink from 'next/link'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import Stack from '@mui/material/Stack'
+import MuiCard from '@mui/material/Card'
+import { FacebookIcon, GoogleIcon } from '@/components/icons'
+import ForgotPassword from '@/components/login/ForgotPassword'
+import { styled } from '@mui/material'
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  width: '100%',
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: 'auto',
+  boxShadow: 'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  [theme.breakpoints.up('sm')]: {
+    width: '450px'
+  },
+  ...theme.applyStyles('dark', {
+    boxShadow: 'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px'
+  })
+}))
+
+const Container = styled(Stack)(({ theme }) => ({
+  height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+  minHeight: '100%',
+  padding: theme.spacing(2),
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(4)
+  },
+  '&::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    zIndex: -1,
+    inset: 0,
+    backgroundImage: 'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+    backgroundRepeat: 'no-repeat',
+    ...theme.applyStyles('dark', {
+      backgroundImage: 'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))'
+    })
+  }
+}))
+
+export default function Login() {
+  const [emailError, setEmailError] = React.useState(false)
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState('')
+  const [passwordError, setPasswordError] = React.useState(false)
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('')
+  const [open, setOpen] = React.useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    if (emailError || passwordError) {
+      event.preventDefault()
+      return
+    }
+    const data = new FormData(event.currentTarget)
+    console.log({
+      email: data.get('email'),
+      password: data.get('password')
+    })
+  }
+
+  const validateInputs = () => {
+    const email = document.getElementById('email') as HTMLInputElement
+    const password = document.getElementById('password') as HTMLInputElement
+
+    let isValid = true
+
+    if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+      setEmailError(true)
+      setEmailErrorMessage('Please enter a valid email address.')
+      isValid = false
+    } else {
+      setEmailError(false)
+      setEmailErrorMessage('')
+    }
+
+    if (!password.value || password.value.length < 6) {
+      setPasswordError(true)
+      setPasswordErrorMessage('Password must be at least 6 characters long.')
+      isValid = false
+    } else {
+      setPasswordError(false)
+      setPasswordErrorMessage('')
+    }
+
+    return isValid
+  }
+
+  return (
+    <>
+      <CssBaseline enableColorScheme />
+      <Container direction='column' justifyContent='space-between'>
+        <Card variant='outlined'>
+          <GoogleIcon />
+          <Typography component='h1' variant='h4' sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}>
+            Log in
+          </Typography>
+          <Box
+            component='form'
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '100%',
+              gap: 2
+            }}
+          >
+            <FormControl>
+              <FormLabel htmlFor='email'>Email</FormLabel>
+              <TextField
+                error={emailError}
+                helperText={emailErrorMessage}
+                id='email'
+                type='email'
+                name='email'
+                placeholder='someone@somewhere.com'
+                autoComplete='email'
+                autoFocus
+                required
+                fullWidth
+                variant='outlined'
+                color={emailError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor='password'>Password</FormLabel>
+              <TextField
+                error={passwordError}
+                helperText={passwordErrorMessage}
+                name='password'
+                placeholder='very secret'
+                type='password'
+                id='password'
+                autoComplete='current-password'
+                autoFocus
+                required
+                fullWidth
+                variant='outlined'
+                color={passwordError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <ForgotPassword open={open} handleClose={handleClose} />
+            <Button type='submit' fullWidth variant='contained' onClick={validateInputs}>
+              Log in
+            </Button>
+            <Link
+              component='button'
+              type='button'
+              onClick={handleClickOpen}
+              variant='body2'
+              sx={{ alignSelf: 'center' }}
+            >
+              Forgot your password?
+            </Link>
+          </Box>
+          <Divider>or</Divider>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <Button fullWidth variant='outlined' onClick={() => alert('Log in with Google')} startIcon={<GoogleIcon />}>
+              Log in with Google
+            </Button>
+            <Button
+              fullWidth
+              variant='outlined'
+              onClick={() => alert('Log in with Facebook')}
+              startIcon={<FacebookIcon />}
+            >
+              Log in with Facebook
+            </Button>
+            <Typography sx={{ textAlign: 'center' }}>
+              Don&apos;t have an account? <NextLink href='/signup'>Sign up</NextLink>
+            </Typography>
+          </Box>
+        </Card>
+      </Container>
+    </>
+  )
+}
