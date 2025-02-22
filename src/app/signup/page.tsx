1b18069e3987c2becb1b22f208fb5ee84ee8cec1
Thanh Nguyen <thanh.nguyencong@go1.com>
2025-02-22 20:45:52 +0700

    Add some pages placeholder

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -9,49 +9,9 @@ import FormLabel from '@mui/material/FormLabel'
 import FormControl from '@mui/material/FormControl'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import Stack from '@mui/material/Stack'
-import MuiCard from '@mui/material/Card'
-import { styled } from '@mui/material/styles'
 import { FacebookIcon, GoogleIcon } from '@/components/icons'
 import Link from 'next/link'
-
-const Card = styled(MuiCard)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignSelf: 'center',
-  width: '100%',
-  padding: theme.spacing(4),
-  gap: theme.spacing(2),
-  margin: 'auto',
-  boxShadow: 'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-  [theme.breakpoints.up('sm')]: {
-    width: '450px'
-  },
-  ...theme.applyStyles('dark', {
-    boxShadow: 'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px'
-  })
-}))
-
-const SignUpContainer = styled(Stack)(({ theme }) => ({
-  height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
-  minHeight: '100%',
-  padding: theme.spacing(2),
-  [theme.breakpoints.up('sm')]: {
-    padding: theme.spacing(4)
-  },
-  '&::before': {
-    content: '""',
-    display: 'block',
-    position: 'absolute',
-    zIndex: -1,
-    inset: 0,
-    backgroundImage: 'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-    backgroundRepeat: 'no-repeat',
-    ...theme.applyStyles('dark', {
-      backgroundImage: 'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))'
-    })
-  }
-}))
+import { Card, Container } from '@/components/auth'
 
 export default function SignUp() {
   const [emailError, setEmailError] = React.useState(false)
@@ -115,7 +75,7 @@ export default function SignUp() {
   return (
     <>
       <CssBaseline enableColorScheme />
-      <SignUpContainer direction='column' justifyContent='space-between'>
+      <Container direction='column' justifyContent='space-between'>
         <Card variant='outlined'>
           <GoogleIcon />
           <Typography component='h1' variant='h4' sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}>
@@ -196,7 +156,7 @@ export default function SignUp() {
             </Typography>
           </Box>
         </Card>
-      </SignUpContainer>
+      </Container>
     </>
   )
 }
---
+++ src/app/wedding/page.tsx
@@ -0,0 +1,3 @@
+export default function Wedding() {
+  return <div>Wedding</div>
+}
