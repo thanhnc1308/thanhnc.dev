37b07ada3bc0458da2672c1e894795420afd4296
Thanh Nguyen <thanh.nguyencong@go1.com>
2025-02-24 16:29:05 +0700

    Fix user avatar dropdown

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,3 +1,4 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import {
   Bars3Icon,
   XMarkIcon,
@@ -59,25 +60,18 @@ export default function AdminLayout({
             </div>
             <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
               {/* Profile dropdown */}
-              <div className='relative ml-3'>
-                <div>
-                  <button className='relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden cursor-pointer'>
-                    <span className='absolute -inset-1.5' />
-                    <span className='sr-only'>Open user menu</span>
-                    <UserCircleIcon aria-hidden='true' className='size-6' />
-                  </button>
-                </div>
-                <div className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in'>
-                  <div>
-                    <a
-                      href='#'
-                      className='block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'
-                    >
+              <Menu>
+                <MenuButton className='relative flex rounded-full bg-gray-800 text-sm cursor-pointer data-[active]:bg-blue-200'>
+                  <UserCircleIcon className='size-6 text-white' />
+                </MenuButton>
+                <MenuItems anchor='bottom'>
+                  <MenuItem>
+                    <div className='block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden'>
                       Sign out
-                    </a>
-                  </div>
-                </div>
-              </div>
+                    </div>
+                  </MenuItem>
+                </MenuItems>
+              </Menu>
             </div>
           </div>
         </div>
